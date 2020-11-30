0a07f3cfacf4c6f2f530887c9d040c6b872d6e18
StevBL <stevbl@ip-192-168-1-201.ec2.internal>
2020-11-30 17:16:42 -0600

    feat: add Pixbay api

--- api/story.js
+++ api/story.js
@@ -1,18 +1,34 @@
 window.onload = function () {
     // Variables
-    const IMAGES = [
-        "./img/001.jpg",
-        "./img/002.png",
-        "./img/003.jpg",
-        "./img/004.png"
-    ];
 
     const TIME_INTERVAL = 2000;
     const btnBack = document.querySelector('#retroceder');
     const btnNext = document.querySelector('#avanzar');
     const img = document.querySelector('#chargeIMG');
+    let IMAGES = [];
     let current = 0;
     let interval;
+
+    // GET IMGS API
+    let API_KEY = '19224548-c145817650eb0d686142a63ed';
+    let URL = 'https://pixabay.com/api/';
+    let q = 'universe';
+    let min_width =	'5000';
+    let min_height = '5000';	
+    let orientation = 'vertical';
+
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(`${URL}?key=${API_KEY}&q=${q}&min_width=${min_width}&min_height=${min_height}&orientation=${orientation}`, requestOptions)
+    .then(response => response.json().then(data => {
+        for(let i = 0; i<(data.hits).length; i++){
+            IMAGES[i] = data.hits[i].previewURL;
+        }
+    }))
+    .catch(error => console.log('error', error));
 
     // Functions
 
